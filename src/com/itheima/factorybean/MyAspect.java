9b33cf8f331a71bd3142f6f2a8509d822c7381ff
pai_d_x <https://github.com/stars12138/homework>
2021-09-22 13:34:55 +0800

    test3

---
+++ src/com/itheima/factorybean/MyAspect.java
@@ -0,0 +1,22 @@
+package com.itheima.factorybean;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class MyAspect implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        check_Permissions();
+        Object obj= methodInvocation.proceed();
+        log();
+        return obj;
+    }
+
+    private void log() {
+        System.out.println("模拟记录日志...");
+    }
+
+    private void check_Permissions() {
+        System.out.println("模拟检查权限...");
+    }
+}
