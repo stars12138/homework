1ed44a4cf127dd4692f7e9209626a932149283b1
pai_d_x <https://github.com/stars12138/homework>
2021-09-22 13:17:56 +0800

    test2

---
+++ src/com/itheima/cglib/CglibProxy.java
@@ -0,0 +1,27 @@
+package com.itheima.cglib;
+
+import com.itheima.aspect.MyAspect;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.invoke.MethodHandleInfo;
+import java.lang.reflect.Method;
+
+public class CglibProxy implements MethodInterceptor {
+        public Object createProxy(Object target){
+            Enhancer enhancer=new Enhancer();
+            enhancer.setSuperclass(target.getClass());
+            enhancer.setCallback(this);
+            return enhancer.create();
+        }
+
+    @Override
+    public Object intercept(Object proxy, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        MyAspect myAspect=new MyAspect();
+        myAspect.check_Permissions();
+        Object obj=methodProxy.invokeSuper(proxy,args);
+        myAspect.log();
+        return obj;
+    }
+}
